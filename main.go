3e4452912dc2f8da5f02744c7543414baa95af1f
MaoenD <guillaume.dorges@ynov.com>
2024-02-02 14:41:45 +0100

    pseudo_main

--- main.go
+++ main.go
@@ -3,13 +3,34 @@ package main
 import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
 	myApp := app.New()
 	myWindow := myApp.NewWindow("Groupie Tracker")
 
-	myWindow.Resize(fyne.NewSize(800, 600))
+	// Champ de recherche
+	searchBar := widget.NewEntry()
+	searchBar.SetPlaceHolder("Search Artists...")
 
+	// Zone d'affichage des résultats de recherche
+	searchResults := widget.NewLabel("Search results appear here")
+
+	// Bouton de recherche
+	searchButton := widget.NewButton("Search", func() {
+		searchResults.SetText("Results for: " + searchBar.Text)
+	})
+
+	// Disposition widget
+	content := container.NewVBox(
+		searchBar,
+		searchButton,
+		searchResults,
+	)
+
+	myWindow.SetContent(content)
+	myWindow.Resize(fyne.NewSize(400, 300)) // ajustement size
 	myWindow.ShowAndRun()
 }
