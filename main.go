ed47eea33c20db1fc6bf4edfaf6eb312d76c0140
Your Name <you@example.com>
2024-02-08 15:16:43 +0100

    Affiche card

--- main.go
+++ main.go
@@ -8,7 +8,6 @@ import (
 	"strings"
 	"time"
 
-	"fyne.io/fyne/theme"
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/canvas"
@@ -33,8 +32,18 @@ func main() {
 	// Exemple de données pour les artistes
 	artists := []Artist{
 		{Name: "Michael Jackson", Image: "public/michaeljackson.jpg", YearStarted: 1964, DebutAlbum: time.Date(1972, time.November, 13, 0, 0, 0, 0, time.UTC), Members: []string{"Michael Jackson"}},
-		{Name: "Queen", Image: "public/queen.png", YearStarted: 1970, DebutAlbum: time.Date(1973, time.July, 13, 0, 0, 0, 0, time.UTC), Members: []string{"Freddie Mercury", "Brian May", "Roger Taylor", "John Deacon"}},
+		{Name: "Queen", Image: "public/queen.jpg", YearStarted: 1970, DebutAlbum: time.Date(1973, time.July, 13, 0, 0, 0, 0, time.UTC), Members: []string{"Freddie Mercury", "Brian May", "Roger Taylor", "John Deacon"}},
 		{Name: "Pink Floyd", Image: "public/pinkfloyd.jpeg", YearStarted: 1965, DebutAlbum: time.Date(1967, time.August, 5, 0, 0, 0, 0, time.UTC), Members: []string{"Syd Barrett", "Roger Waters", "Richard Wright", "Nick Mason"}},
+		{Name: "The Beatles", Image: "public/thebeatles.jpg", YearStarted: 1960, DebutAlbum: time.Date(1963, time.March, 22, 0, 0, 0, 0, time.UTC), Members: []string{"John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"}},
+		{Name: "Elvis Presley", Image: "public/elvispresley.jpg", YearStarted: 1954, DebutAlbum: time.Date(1956, time.March, 23, 0, 0, 0, 0, time.UTC), Members: []string{"Elvis Presley"}},
+		{Name: "The Rolling Stones", Image: "public/therollingstones.jpg", YearStarted: 1962, DebutAlbum: time.Date(1964, time.April, 17, 0, 0, 0, 0, time.UTC), Members: []string{"Mick Jagger", "Keith Richards", "Charlie Watts", "Ronnie Wood"}},
+		{Name: "Led Zeppelin", Image: "public/ledzeppelin.jpg", YearStarted: 1968, DebutAlbum: time.Date(1969, time.January, 12, 0, 0, 0, 0, time.UTC), Members: []string{"Robert Plant", "Jimmy Page", "John Paul Jones", "John Bonham"}},
+		{Name: "AC/DC", Image: "public/acdc.jpg", YearStarted: 1973, DebutAlbum: time.Date(1975, time.February, 17, 0, 0, 0, 0, time.UTC), Members: []string{"Angus Young", "Brian Johnson", "Phil Rudd", "Cliff Williams", "Stevie Young"}},
+		{Name: "Nirvana", Image: "public/nirvana.jpg", YearStarted: 1987, DebutAlbum: time.Date(1989, time.June, 15, 0, 0, 0, 0, time.UTC), Members: []string{"Kurt Cobain", "Krist Novoselic", "Dave Grohl"}},
+		{Name: "The Beach Boys", Image: "public/thebeachboys.jpg", YearStarted: 1961, DebutAlbum: time.Date(1962, time.October, 1, 0, 0, 0, 0, time.UTC), Members: []string{"Brian Wilson", "Mike Love", "Al Jardine", "Bruce Johnston", "David Marks"}},
+		{Name: "The Who", Image: "public/thewho.jpg", YearStarted: 1964, DebutAlbum: time.Date(1965, time.December, 3, 0, 0, 0, 0, time.UTC), Members: []string{"Roger Daltrey", "Pete Townshend", "John Entwistle", "Keith Moon"}},
+		{Name: "David Bowie", Image: "public/davidbowie.jpg", YearStarted: 1962, DebutAlbum: time.Date(1967, time.June, 1, 0, 0, 0, 0, time.UTC), Members: []string{"David Bowie"}},
+		{Name: "Metallica", Image: "public/metallica.jpg", YearStarted: 1981, DebutAlbum: time.Date(1983, time.July, 25, 0, 0, 0, 0, time.UTC), Members: []string{"James Hetfield", "Lars Ulrich", "Kirk Hammett", "Robert Trujillo"}},
 	}
 
 	// Champ de recherche
@@ -80,20 +89,33 @@ func main() {
 	}
 
 	// Création du conteneur pour afficher les artistes
-	artistsContainer := container.NewHBox()
+	artistsContainer := container.NewVBox()
 
-	// Ajouter les cartes des artistes dans le conteneur
-	for _, artist := range artists {
-		card := createCardGeneralInfo(artist)
-		artistsContainer.Add(card)
+	// Ajouter les cartes des artistes par groupes de 3 dans des conteneurs de grille
+	for i := 0; i < len(artists); i += 3 {
+		// Créer un nouveau conteneur de grille pour chaque ligne d'artistes
+		rowContainer := container.NewGridWithColumns(3)
+		// Ajouter les trois artistes de cette ligne dans la grille
+		for j := i; j < i+3 && j < len(artists); j++ {
+			card := createCardGeneralInfo(artists[j])
+			rowContainer.Add(card)
+		}
+		// Ajouter un espacement après chaque ligne
+		artistsContainer.Add(rowContainer)
+		artistsContainer.Add(layout.NewSpacer()) // Ajouter un espacement
+		artistsContainer.Add(layout.NewSpacer()) // Ajouter un espacement
 	}
+
+	// Créer un conteneur de défilement pour le conteneur principal
+	scrollContainer := container.NewVScroll(artistsContainer)
+	scrollContainer.SetMinSize(fyne.NewSize(1080, 720)) // Taille minimale pour activer le défilement
 
 	// Création du conteneur principal avec la couleur de fond spécifiée
 	content := container.NewVBox(
 		searchBar,
 		searchButton,
 		searchResults,
-		artistsContainer, // Utilisation du conteneur HBox pour afficher les cartes côte à côte
+		scrollContainer, // Utilisation du conteneur de défilement pour les artistes
 	)
 
 	// Centrer les cartes dans la fenêtre
@@ -113,57 +135,6 @@ func main() {
 	myWindow.SetContent(backgroundContainer)
 	myWindow.Resize(fyne.NewSize(1080, 720)) // ajustement size
 	myWindow.ShowAndRun()
-}
-
-func createCard(artist Artist) fyne.CanvasObject {
-	// Redimensionner l'image
-	image := canvas.NewImageFromFile(artist.Image)
-	image.FillMode = canvas.ImageFillContain
-	image.SetMinSize(fyne.NewSize(120, 120))
-	image.Resize(fyne.NewSize(120, 120))
-
-	// Nom de l'artiste en gras et plus gros
-	nameLabel := canvas.NewText(artist.Name, theme.TextColor())
-
-	// Date de début de l'artiste en plus petit
-	yearLabel := canvas.NewText(fmt.Sprintf("Year Started: %d", artist.YearStarted), theme.TextColor())
-
-	// Album de début de l'artiste
-	debutLabel := canvas.NewText(fmt.Sprintf("Debut Album: %s", artist.DebutAlbum.Format("2006-01-02")), theme.TextColor())
-
-	// Créer le conteneur pour les informations sur l'artiste
-	infoContainer := container.New(layout.NewVBoxLayout(),
-		layout.NewSpacer(),
-		nameLabel,
-		layout.NewSpacer(),
-		container.NewHBox(
-			yearLabel,
-			layout.NewSpacer(),
-			debutLabel,
-		),
-		layout.NewSpacer(),
-		layout.NewSpacer(),
-	)
-
-	// Définir la taille fixe pour le conteneur d'informations
-	infoContainer.Resize(fyne.NewSize(300, 180))
-
-	// Créer le conteneur pour la carte de l'artiste
-	cardContent := container.New(layout.NewBorderLayout(nil, nil, nil, nil), image, infoContainer)
-	cardContent.Resize(fyne.NewSize(300, 300))
-
-	// Créer un rectangle pour le contour avec des coins arrondis
-	border := canvas.NewRectangle(color.Transparent) // Définir une couleur transparente pour le remplissage
-	border.SetMinSize(fyne.NewSize(300, 300))
-	border.Resize(fyne.NewSize(296, 296)) // Redimensionner légèrement la bordure pour inclure les coins arrondis
-	border.StrokeColor = color.Black      // Définir la couleur de la bordure
-	border.StrokeWidth = 3                // Définir l'épaisseur de la bordure
-	border.CornerRadius = 20              // Définir les coins arrondis
-
-	// Ajouter le rectangle de contour à la carte
-	cardContent.AddObject(border)
-
-	return cardContent
 }
 
 func createCardGeneralInfo(artist Artist) fyne.CanvasObject {
